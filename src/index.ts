1efed8bb878d78ce6333e3f534941cfd30930249
mai.shu <ansiboy@163.com>
2020-07-12 11:13:50 +0800

    添加导出

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Settings } from "./settings";
 export { WebServer } from "./web-server";
+export { VirtualDirectory } from "./virtual-directory";
+export { RequestProcessor, RequestContext, ExecuteResult, Content } from "./request-processor";
