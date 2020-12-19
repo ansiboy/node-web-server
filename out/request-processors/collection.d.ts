ccd5078483f633115804246f3176c9a6084a5f13
mai.shu <ansiboy@163.com>
2020-12-19 13:22:37 +0800

    push 改为 add

--- out/request-processors/collection.d.ts
+++ out/request-processors/collection.d.ts
@@ -5,7 +5,7 @@ declare type RequestProcessorType<T extends RequestProcessor> = {
 export declare class RequestProcessorTypeCollection {
     private items;
     constructor(items?: RequestProcessor[]);
-    push(item: RequestProcessor): void;
+    add(item: RequestProcessor): void;
     foreach(func: (item: RequestProcessor) => Promise<any | void>): Promise<void>;
     item(index: number): RequestProcessor;
     get length(): number;
