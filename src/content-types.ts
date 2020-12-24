d067bd2f5315426c8de5b29a9cb8c7f9b44c5cdc
mai.shu <ansiboy@163.com>
2020-12-24 18:26:37 +0800

    定义 defaultContentTypes 数据类型

--- src/content-types.ts
+++ src/content-types.ts
@@ -1,5 +1,5 @@
 const encoding = 'UTF-8'
-export const defaultContentTypes = {
+export const defaultContentTypes: { [key: string]: string } = {
     ".json": `application/json; charset=${encoding}`,
     ".txt": `text/plain; charset=${encoding}`,
     ".html": `text/html; charset=${encoding}`,
