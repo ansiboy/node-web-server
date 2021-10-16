593ce6612102e53f546cf2034148f255320807ac
mai.shu <ansiboy@163.com>
2021-10-17 04:17:08 +0800

    fixed bug

--- demo/nws-config.js
+++ demo/nws-config.js
@@ -1,8 +1,15 @@
 const path = require("path");
-
+const encoding = 'UTF-8'
 module.exports = {
     "port": 4612,
-    "virtualPath": {
-        "static": path.join(__dirname, "../out/static")
+    virtualPaths: {
+        // "static": path.join(__dirname, "../out/static")
+    },
+    processors: {
+        StaticFile: {
+            contentTypes: {
+                ".ts": `application/x-javascript; charset=${encoding}`,
+            }
+        }
     }
 }
